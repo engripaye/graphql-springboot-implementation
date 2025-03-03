4367c1f9685d4b70c882bfe9efdfcb91f0bdda3c
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-03-03 10:04:29 +0100

    update, create, and delete service on graphql

--- src/main/java/org/engripaye/graphqlspringbootimplementation/controller/PlayerController.java
+++ src/main/java/org/engripaye/graphqlspringbootimplementation/controller/PlayerController.java
@@ -1,11 +1,15 @@
 package org.engripaye.graphqlspringbootimplementation.controller;
 
 import org.engripaye.graphqlspringbootimplementation.model.Player;
+import org.engripaye.graphqlspringbootimplementation.model.Team;
 import org.engripaye.graphqlspringbootimplementation.service.PlayerService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PlayerController {
@@ -19,5 +23,25 @@ public class PlayerController {
     @QueryMapping
     public List<Player> findAll() {
         return playerService.findAll();
+    }
+
+    @QueryMapping
+    public Optional<Player> findById(@Argument Integer id) {
+        return playerService.findById(id);
+    }
+
+    @MutationMapping
+    public Player createPlayers(@Argument String name, @Argument Team team) {
+        return playerService.createPlayers(name, team);
+    }
+
+    @MutationMapping
+    public Player updatePlayers(@Argument Integer id, @Argument String name, @Argument Team team){
+        return playerService.updatePlayer(id, name, team);
+    }
+
+    @MutationMapping
+    public Player deletePlayers(@Argument Integer id){
+        return playerService.deletePlayer(id);
     }
 }
